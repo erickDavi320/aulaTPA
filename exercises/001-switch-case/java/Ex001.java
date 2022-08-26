bcccfa24341a93d9cf198e7c6798fdf8abac1141
yotozangue <allandelima64@hotmail.com>
2022-08-26 16:12:21 -0300

    Ex001 - Switch Case 001 🤡

--- exercises/001-switch-case/java/Ex001.java
+++ exercises/001-switch-case/java/Ex001.java
@@ -1,4 +1,42 @@
 
+/*
+    1-Uma escola de futebol classifica seus alunos da seguinte forma:
+        • Alunos de 6 anos são categoria “dente de leite”
+        • Alunos de 7 anos são categoria “júnior”
+        • Alunos de 8 anos são categoria “júnior max”
+        • Alunos de 9 anos são categoria “júnior master”
+        • Alunos de 10 anos são categoria “master”
+*/
+
+import java.io.*;
+
 public class Ex001 {
-    
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new
+                BufferedReader(new InputStreamReader(System.in));
+
+        int idade;
+        String category = "", result;
+        boolean status = true;
+
+        System.out.println("Digite a sua idade:");
+        idade = Integer.parseInt(in.readLine());
+
+        switch (idade) {
+            case 6  -> category    = "dente de leite";
+            case 7  -> category    = "júnior";
+            case 8  -> category    = "júnior max";
+            case 9  -> category    = "júnior master";
+            case 10 -> category    = "master";
+            default -> status       = false;
+        }
+
+        result = (status)
+                ? "A sua categoria é " + category
+                :"A sua faixa de idade não é atendida. :/";
+
+        System.out.println(result);
+    }
 }
