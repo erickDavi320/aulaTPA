21282222ffd73ab581ae222f497d9e851595d7be
erickDavi320 <112183839+erickDavi320@users.noreply.github.com>
2022-09-02 02:47:42 -0300

    Update and rename ProgramaDoSigno.java to EX002.java

--- exercises/001-switch-case/java/EX002.java
+++ exercises/001-switch-case/java/EX002.java
@@ -1,8 +1,6 @@
-package teste_tpa;
-
 import java.util.Scanner;
 
-public class ProgramaDoSigno {
+public class Ex002 {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		int dia,mes;
