543f42bd6eca134b650021d5c2f72cc0134a3b32
Allan Lima <allandelima64@hotmail.com>
2022-09-15 17:30:19 -0300

    🐅 Ex003 - While Loop 003

---
+++ exercises/003-while-loop/java/Ex003.java
@@ -0,0 +1,12 @@
+public class Ex003 {
+
+    public static void main(String[] args) {
+        int cont = 1;
+        int result = 0;
+        while (cont <= 100) {
+            result += cont;
+            cont++;
+        }
+        System.out.println(result);
+    }
+}
