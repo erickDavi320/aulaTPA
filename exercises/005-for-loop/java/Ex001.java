203da1ebf3d6cc60165a5562b48f7fa08b24a99e
Erick David <erick.silva320@etec.sp.gov.br>
2022-11-04 10:22:11 -0300

    💫 Ex001 - For Loop 005

---
+++ exercises/005-for-loop/java/Ex001.java
@@ -0,0 +1,14 @@
+public class Ex001 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            
+            if(i % 10 == 0) {
+                System.out.println(i + " eh multiplo de 10");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
