47cc78bdb7edd99722716b4a5710f5fe33e9c407
erickDavi320 <112183839+erickDavi320@users.noreply.github.com>
2022-11-11 11:25:52 -0300

    Add files via upload

---
+++ exercises/006-classicos/java/Ex003.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ex003 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int num;
+		boolean primo = true;
+		
+		System.out.print("Informe o valor a ser testado:");
+		num = in.nextInt();
+		
+		for(int check = 2;check <= num; check++) {
+			if(num % check == 0 && num != check){
+				
+			primo = false;
+				
+			}
+		}
+		if(primo == true){
+			System.out.println("O número ["+num+"] é primo :D");
+		}else if(primo == false) {
+			System.out.println("O número ["+num+"] NÃO é primo ;-;");
+		}
+		
+		
+		in.close();
+	}
+
+}
